f7bb42c668834b9111a9e6bb1d1d51f646977ca0
jeemyeong <jeemyeong@gmail.com>
2019-08-30 12:54:12 +0900

    Add filter

--- src/createMiddleware.ts
+++ src/createMiddleware.ts
@@ -1,22 +1,27 @@
 import { Action, Middleware } from 'redux';
 
 interface Options {
-  confirm?: () => boolean | (() => Promise<boolean>);
+  confirm?: () => boolean | Promise<boolean>;
+  filter?: (action: Action) => boolean | Promise<boolean>;
 }
 
-const defaultOptions: Options = {
+const defaultOptions = {
   confirm: () => window.confirm('Are you sure?'),
+  filter: () => true,
 };
 
 export default (rawOptions?: Options): Middleware => {
   const options = { ...defaultOptions, ...rawOptions };
-  const { confirm } = options;
+  const { confirm, filter } = options;
 
   return () => next => async (action: Action) => {
-    // @ts-ignore
-    const answer = await confirm();
-
-    if (answer) {
+    const filtered = await filter(action);
+    if (filtered) {
+      const answer = await confirm();
+      if (answer) {
+        next(action);
+      }
+    } else {
       next(action);
     }
   };
